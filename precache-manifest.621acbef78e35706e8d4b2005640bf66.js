431bfd7544e51cb1aaab09baa563b67f6a148971
Małgorzata Grajewska <malgorzata.grajewska@hotmail.co.kr>
2020-01-27 21:35:29 +0100

    Updates

---
+++ precache-manifest.621acbef78e35706e8d4b2005640bf66.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "d09eebb64f0dc77b13c4b967dccd654e",
+    "url": "/miedzyslowami/jetcake-project/index.html"
+  },
+  {
+    "revision": "f515338d490f98e0bed2",
+    "url": "/miedzyslowami/jetcake-project/static/css/2.2b6691fa.chunk.css"
+  },
+  {
+    "revision": "45256d9f2424d034deb5",
+    "url": "/miedzyslowami/jetcake-project/static/css/main.05cd2b12.chunk.css"
+  },
+  {
+    "revision": "f515338d490f98e0bed2",
+    "url": "/miedzyslowami/jetcake-project/static/js/2.e3929e0c.chunk.js"
+  },
+  {
+    "revision": "f032203ca460334c00de541c30a6078a",
+    "url": "/miedzyslowami/jetcake-project/static/js/2.e3929e0c.chunk.js.LICENSE"
+  },
+  {
+    "revision": "45256d9f2424d034deb5",
+    "url": "/miedzyslowami/jetcake-project/static/js/main.f19291b2.chunk.js"
+  },
+  {
+    "revision": "472ec67fa1c90115a200",
+    "url": "/miedzyslowami/jetcake-project/static/js/runtime-main.c9a0b304.js"
+  },
+  {
+    "revision": "15a2aed1cfcf38cbc74aa910715ab16f",
+    "url": "/miedzyslowami/jetcake-project/static/media/daylight-environment-forest-idyllic-459225.15a2aed1.jpg"
+  }
+]);
