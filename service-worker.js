8ef815b7a34600e350ebc48ce1f01ae2e01e09ef
Małgorzata Grajewska <malgorzata.grajewska@hotmail.co.kr>
2020-02-01 14:09:56 +0100

    Updates

--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/jetcake-project/precache-manifest.726399eebbc02c7e0c9cd921d8c52896.js"
+  "/jetcake-project/precache-manifest.a9412722c12f8606f93b1dcee6c06911.js"
 );
 
 self.addEventListener('message', (event) => {
